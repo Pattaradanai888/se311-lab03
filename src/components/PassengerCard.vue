fbe9b1213fe0b1c670dc389c8b20fe8ef056cd17
Pattaradanai888 <pattaradanai_t@cmu.ac.th>
2024-07-17 20:33:46 +0700

    Add axios to fecth data and show on homeView

---
+++ src/components/PassengerCard.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { defineProps } from 'vue'
+import type { Passenger } from '@/type'
+defineProps<{
+  passenger: Passenger
+}>()
+</script>
+
+<template>
+  <div class="event-class">
+    <div class="event-type">
+      <h2>{{ passenger.first_name }} {{ passenger.last_name }}</h2>
+      <span>{{ passenger.id }}</span>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.event-type {
+  padding: 20px;
+  width: 250px;
+  cursor: pointer;
+  border: 1px solid #39495c;
+  margin-bottom: 18px;
+  text-align: center;
+}
+.event-type:hover {
+  transform: scale(1.01);
+  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,36 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import PassengerCard from '@/components/PassengerCard.vue'
+import type { Passenger } from '@/types'
+import { ref, onMounted } from 'vue'
+import axios from 'axios'
+
+const passengers = ref<Passenger[]>(null)
+onMounted(() => {
+  axios
+    .get('https://my-json-server.typicode.com/se331-2022/passengerdb/passenger?_page=1&_limit=5')
+    .then((response) => {
+      passengers.value = response.data
+    })
+    .catch((error) => {
+      console.error('There was an error!', error)
+    })
+})
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <h1>Events For Good</h1>
+  <!-- new element -->
+  <div class="container">
+    <div>
+      <PassengerCard v-for="passenger in passengers" :key="passenger.id" :passenger="passenger" />
+    </div>
+  </div>
 </template>
+
+<style scoped>
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+}
+</style>
